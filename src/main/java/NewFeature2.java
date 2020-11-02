c0b7f424fe3da49aa7c19559fff4321fa983d161
Bohdan <b.chernov.kiev@gmail.com>
2020-11-02 06:36:52 +0200

    Development of new Feature2.

---
+++ src/main/java/NewFeature2.java
@@ -0,0 +1,9 @@
+public class NewFeature2 {
+    public static void main(String[] args) {
+        System.out.println("newFeature2 change 1");
+        System.out.println("newFeature2 change 2");
+        System.out.println("newFeature2 change 3");
+        System.out.println("newFeature2 change 4");
+        System.out.println("newFeature2 change 5");
+    }
+}
