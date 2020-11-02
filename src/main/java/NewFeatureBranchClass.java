b99d1c8508889ec918248e6a85388ccb53f2e7ad
Bohdan <b.chernov.kiev@gmail.com>
2020-11-02 06:16:20 +0200

    Squash commit from New Feature

---
+++ src/main/java/NewFeatureBranchClass.java
@@ -0,0 +1,8 @@
+public class NewFeatureBranchClass {
+    public static void main(String[] args) {
+        System.out.println("newFeature change1");
+        System.out.println("newFeature change2");
+        System.out.println("newFeature change3");
+        System.out.println("newFeature change4");
+    }
+}
